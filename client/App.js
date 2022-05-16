0764f5748730a148b00fa7d0a9ae28cf18c5806e
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-05-15 20:51:06 -0400

    set up ml5

--- client/App.js
+++ client/App.js
@@ -1,32 +1,62 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import Users from './Components/Users';
+// Importing ml5.js as ml5
+import * as ml5 from "ml5";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            users: [],
-            loading: true
-        }
+    state = {
+        predictions: []  // Set the empty array predictions state
     }
 
-    async componentDidMount() {
-        console.log('do you likes me?')
+    setPredictions = (pred) => {
+        // Set the prediction state with the model predictions
         this.setState({
-            users: (await axios.get('/api/users')).data,
-            loading: false
+            predictions: pred
         });
     }
 
+    classifyImg = () => {
+        // Initialize the Image Classifier method with MobileNet
+        const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+        // When the model is loaded
+        function modelLoaded() {
+            console.log('Model Loaded!');
+        }
+        // Put the image to classify inside a variable
+        const image = document.getElementById('image');
+        // Make a prediction with a selected image
+        classifier.predict(image, 5, function (err, results) {
+            // Return the results
+            return results;
+        })
+            .then((results) => {
+                // Set the predictions in the state
+                this.setPredictions(results)
+            })
+    }
+
+    componentDidMount() {
+        // once the component has mount, start the classification
+        this.classifyImg();
+    }
+
     render() {
-        const { users, loading } = this.state;
-        if (loading) {
-            return '....loading';
+        // First set the predictions to a default value while loading
+        let predictions = (<div className="loader"></div>);
+        // Map over the predictions and return each prediction with probability
+        if (this.state.predictions.length > 0) {
+            predictions = this.state.predictions.map((pred, i) => {
+                let { className, probability } = pred;
+                // round the probability with 2 decimal
+                probability = Math.floor(probability * 10000) / 100 + "%";
+                return (
+                    <div key={i + ""}>{i + 1}. Prediction: {className} at {probability} </div>
+                )
+            })
         }
         return (
             <div>
-                <Users users={ users }/>
+                <img src='/public/tiger.jpg' id="image" ></img>
+                { predictions }
             </div>
         )
     }
